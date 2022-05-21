b161e21e8bb13c435aa3bb376ab986d337b28dfc
DerickMacbookAir <derickdev6@gmail.com>
2022-05-21 17:59:11 -0500

    Using templates now

--- heyworld.py
+++ heyworld.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, abort
 import sqlite3
+
 app = Flask(__name__)
+
 
 def get_user_profile(user_id):
     # Connects to DB
@@ -32,17 +34,67 @@ def get_user_profile(user_id):
     if sqliteConnection:
         sqliteConnection.close()
         print("-----------------------------DB Disconnected")
-    # Returning results (can be empty array)
+        # Returning results (can be empty array)
         return user
+
+
+def get_all_users():
+    # Connects to DB
+    sqliteConnection = sqlite3.connect('usersDB.sqlite3')
+    cursor = sqliteConnection.cursor()
+    # Print statement and execution
+    print("-----------------------------Getting all users ")
+    request = "SELECT * FROM Users ORDER BY first_name"
+    users = []
+    try:
+        cursor.execute(request)
+    except Exception as e:
+        print("Error executing get_all_users " + str(e))
+    records = cursor.fetchall()
+    if len(records) != 0:
+        print("Users found: " + str(len(records)))
+        for user in records:
+            # print(str(user))
+            new_user = {
+                "user_id": user[0],
+                "first_name": user[1],
+                "last_name": user[2],
+                "email": user[3],
+                "gender": user[4],
+                "avatar": user[5],
+            }
+            users.append(new_user)
+
+    else:
+        print("No user found")
+    cursor.close()
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("-----------------------------DB Disconnected")
+        # Returning results (can be empty array)
+        return users
+
 
 @app.route('/')
 def index():
+    # abort(401)
+    return redirect(url_for('user', user_id='derickrp6'))
     return "Hello world"
+
 
 @app.route('/user/<user_id>', methods=['GET'])
 def user(user_id):
     userdata = get_user_profile(user_id)
-    return str(userdata)
+    return userdata
+
+
+@app.route('/users/', methods=['GET'])
+def users():
+    usersdata = get_all_users()
+    return render_template('users.html',
+                           userlist=usersdata,
+                           listlen=len(usersdata))
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -57,4 +109,4 @@ def register():
     print(url_for('user', user_id=new_user['user_id']))
     print(new_user)
     print(request.form)
-    return "Working"
+    return new_user
